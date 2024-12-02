2e205e38a64e624d5436d1eafd8d66c63d8c02a5
Joao Vasconcelos <jvsn19@gmail.com>
2024-12-02 05:53:35 +0000

    add question 1346

---
+++ leetcode/questions/1346/python/1346.py
@@ -0,0 +1,11 @@
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        prevs = set()
+
+        for num in arr:
+            if num * 2 in prevs or num / 2 in prevs:
+                return True
+
+            prevs.add(num)
+        
+        return False
